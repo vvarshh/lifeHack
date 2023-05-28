ffae86a9c33626d57ba2605dd237fdac653c13d3
kishorkumar11 <h.kishorkumar7@gmail.com>
2023-05-28 14:53:40 +0800

    Add prettier

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+          <header>
+        <a href="#" class="logo">COC-STATS</a>
+
+        <ul class="navlist">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Clans</a></li>
+            <li><a href="#">Town Hall</a></li>
+            <li><a href="#">Troops</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+
+        <div class="bx bx-menu" id="menu-icon"></div>
+    </header>
+    </div>
+  )
+}
+
+export default Navbar
