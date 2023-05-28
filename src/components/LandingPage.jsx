edd2d4be2e6164e883f9e19dd6281de0623ea484
kishorkumar11 <h.kishorkumar7@gmail.com>
2023-05-28 15:38:24 +0800

    Add basic navbar

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
-  )
-}
+    <div>
+      <header>
+        <a href="#" class="logo">
+          TravelTalk
+        </a>
 
-export default LandingPage
+        <ul class="navlist">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Settings</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+
+        <div class="bx bx-menu" id="menu-icon"></div>
+      </header>
+    </div>
+  );
+};
+
+export default LandingPage;
