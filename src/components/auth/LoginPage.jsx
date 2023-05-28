620e95722c1cab52cb9d606df9ce973134c47bc8
Shawn Ng <shawn.nghh@gmail.com>
2023-05-28 18:54:58 +0800

    "Tried fixing auth"

--- src/components/auth/LoginPage.jsx
+++ src/components/auth/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { auth } from "../../firebase";
 import './loginPage.css';
@@ -6,8 +6,11 @@ import './loginPage.css';
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [location, setLocation] = useState("");
-  const [interest1, setInterest1] = useState("");
+  const [selectedOption1, setSelectedOption1] = useState("");
+  const [selectedOption2, setSelectedOption2] = useState("");
+  const [selectedOption3, setSelectedOption3] = useState("");
   const [signUpView, setSignUpView] = useState(false);
 
   const signIn = (a) => {
@@ -21,44 +24,84 @@ const SignIn = () => {
       });
   };
 
+  const signUp = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      setPassword("");
+      setConfirmPassword("");
+      return;
+    }
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+        const user = userCredential.user;
+      // Update the user's profile with additional information
+      user.updateProfile({
+        location: location,
+        interest1: selectedOption1,
+        interest2: selectedOption2,
+        interest3: selectedOption3
+      });
+      console.log('User signed up and profile updated successfully:', user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="sign-in-container">
-      <form onSubmit={signIn}>
+      <form onSubmit={signUpView ? signUp : signIn}>
         <h1>Log In to your Account</h1>
         <input
           type="email"
           placeholder="Enter your email"
           value={email}
+          required="true"
           onChange={(e) => setEmail(e.target.value)}
         ></input>
         <input
           type="password"
           placeholder="Enter your password"
           value={password}
+          required="true"
           onChange={(e) => setPassword(e.target.value)}
         ></input>
         {signUpView ? (
           <div>
+            <input
+              type="password"
+              placeholder="Enter your password again"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></input>
             <input
               type="Location"
               placeholder="Enter your location (city)"
               value={location}
               onChange={(e) => setLocation(e.target.value)}
             ></input>
-            <input
-              type="Interest"
-              placeholder="Interest 1"
-              value={interest1}
-              onChange={(e) => setInterest1(e.target.value)}
-            ></input>
-            <button type="submit">Submit</button>
+            <DropdownMenu
+              selectedOption={selectedOption1}
+              setSelectedOption={setSelectedOption1}
+            />
+            <DropdownMenu
+              selectedOption={selectedOption2}
+              setSelectedOption={setSelectedOption2}
+            />
+            <DropdownMenu
+              selectedOption={selectedOption3}
+              setSelectedOption={setSelectedOption3}
+            />
+            <button type="submit">Sign Up</button>
           </div>
         ) : (
           <div>
             <button type="submit">Log In</button>
             <text>or</text>
             <button type="button" onClick={() => setSignUpView(true)}>
-              Sign up
+              Create Account
             </button>
           </div>
         )}
@@ -68,3 +111,21 @@ const SignIn = () => {
 };
 
 export default SignIn;
+
+const DropdownMenu = ({ selectedOption, setSelectedOption }) => {
+  const handleSelectChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  return (
+    <div>
+      <select value={selectedOption} onChange={handleSelectChange}>
+        <option value="">Choose an option</option>
+        <option value="option1">Food</option>
+        <option value="option2">Outdoor & Wildlife</option>
+        <option value="option3">Urban exploration</option>
+        <option value="option4">History & Culture</option>
+      </select>
+    </div>
+  );
+};
---
+++ src/components/profile selection/InterestDropdownMenu.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const DropdownMenu = () => {
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleSelectChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  return (
+    <div>
+      <h3>Select an option:</h3>
+      <select value={selectedOption} onChange={handleSelectChange}>
+        <option value="">Choose an option</option>
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+      </select>
+      {selectedOption && <p>You selected: {selectedOption}</p>}
+    </div>
+  );
+};
+
+export default DropdownMenu;
