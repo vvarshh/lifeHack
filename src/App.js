99a8ba3dba6a71791a029a11f87f5a06d01500fc
Varsha Ramkumar <101083930+vvarshh@users.noreply.github.com>
2023-05-29 18:03:03 +0800

    login and user creation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LoginPage from '../src/components/auth/LoginPage';
 import LandingPage from '../src/components/LandingPage';
 import LocalPage from './components/LocalPage';
 import TouristPage from './components/TouristPage';
+import SignUpPage from './components/auth/SignUpPage';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<LandingPage />} />
+          <Route path="/home/*" element={<LandingPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
           <Route path="/home/local" element={<LocalPage />} />
           <Route path="/home/tourist" element={<TouristPage />} />
         </Routes>
