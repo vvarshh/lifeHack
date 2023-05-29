e41d7a6e8115f05489acee0ba8bc21b0634330d7
Shawn Ng <shawn.nghh@gmail.com>
2023-05-29 22:08:19 +0800

    added structure for chat

---
+++ src/components/chat/ChatPage.jsx
@@ -0,0 +1,10 @@
+import { auth } from "../../firebase";
+import "./App.css";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+
+function ChatPage() {
+    
+}
+
+export default ChatPage;
